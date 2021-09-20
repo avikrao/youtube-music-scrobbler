93e408f09c35fa1004debaf275de158830031090
AvikRao <avik.rao@gmail.com>
2021-09-20 17:32:12 -0400

    Add console logs for timestamps and new scrobbles

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from ytmusicapi import YTMusic
+from datetime import datetime
+from pytz import timezone
 import pylast
 import json
 import time
 
+tz = timezone('US/Eastern')
 ytm = YTMusic("headers_auth.json")
 
 with open("lastfm_auth.json", "r") as f :
@@ -25,6 +28,7 @@ while True :
     raw_history = ytm.get_history()[:5]
     history = [{"title": song["title"], "artist": song["artists"][0]["name"], "album": song["album"]["name"] } for song in raw_history]
 
+    print(datetime.now(tz))
     for track in history[:3] :
         print(track)
     
@@ -37,6 +41,7 @@ while True :
     most_recent = [most_recent_track["title"].strip(), most_recent_track["artist"].strip(), most_recent_track["album"].strip()]
 
     if most_recent != last_scrobble :
+        print("New song scrobbled!")
         network.scrobble(title=most_recent[0], artist=most_recent[1], album=most_recent[2], timestamp=time.time())
         with open("last_scrobble.log", "w") as f :
             f.writelines([stat + "\n" for stat in most_recent])
